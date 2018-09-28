292c200f47c18366a7a1d92b2ff1c8f68caa6212
Kamran Syed <lovelykamran@gmail.com>
2018-09-28 15:02:32 +0500

    Update testing code to use AndroidX and Instrumented Testing.

---
+++ app/src/main/java/com/kamransyed/find_devices_on_wifi/AppInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.kamransyed.find_devices_on_wifi;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class AppInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.kamransyed", appContext.getPackageName());
+    }
+}
